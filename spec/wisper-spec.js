af22131eb393bf5180d9ed18bad516f86ebbfb67
toejough <toejough@gmail.com>
2014-07-31 21:46:51 -0600

    0.1.0 - Added initial source and tests.  Tests are passing.

---
+++ spec/wisper-spec.js
@@ -0,0 +1,69 @@
+// Wisper Tests
+
+
+// [ Pragmas ]
+'use strict';
+/* global require */
+/* global describe */
+/* global it */
+/* global beforeEach */
+/* global expect */
+
+
+// [ Requires ]
+var wisper = require('../wisper.js');
+
+
+// [ Descriptions ]
+describe('local pub/sub', function() {
+    // vars
+    var service;
+    // setup/teardown
+    beforeEach(function() {
+        service = wisper.create_service();
+        return null;
+    });
+    // tests
+    it('allows local publish', function(done) {
+        expect(service.publish).toBeDefined();
+        service.publish({
+            'channel': 'foo',
+            'message': 'bar'
+        }, function(error) {
+            expect(error).toBeFalsy();
+            done();
+            return null;
+        });
+        return null;
+    });
+    it('allows local subscribe', function(done) {
+        expect(service.subscribe).toBeDefined();
+        service.subscribe('foo', function(error) {
+            expect(error).toBeFalsy();
+            done();
+            return null;
+        });
+        return null;
+    });
+    it('delivers publications to subscribers', function(done) {
+        service.subscribe('foo', function(error) {
+            expect(error).toBeFalsy();
+            return null;
+        });
+        service.on('publication', function(publication) {
+            expect(publication).toBeTruthy();
+            expect(publication.channel).toEqual('foo');
+            expect(publication.message).toEqual('bar');
+            done();
+            return null;
+        });
+        service.publish({
+            'channel': 'foo',
+            'message': 'bar'
+        }, function(error) {
+            expect(error).toBeFalsy();
+            return null;
+        });
+        return null;
+    });
+});
---
+++ wisper.js
@@ -0,0 +1,58 @@
+// Wisper
+
+
+// [ Pragmas ]
+'use strict';
+/* global require */
+/* global exports */
+
+
+// [ Requires ]
+// [ -Node- ]
+var events = require('events');
+var util = require('util');
+// [ -Third Party- ]
+var _ = require('underscore');
+
+
+// [ Service ]
+function Service() {
+    // [ -Private Vars- ]
+    // self is always the service instance
+    var self = this;
+    // channels subscribed to
+    var subscriptions = [];
+
+    // [ -Inheritance- ]
+    events.EventEmitter.call(self);
+
+    // [ -Public- ]
+    self.publish = function publish(kwargs, callback) {
+        var channel = kwargs.channel;
+        var message = kwargs.message;
+        if (_.contains(subscriptions, channel)) {
+            self.emit('publication', {
+                'channel': channel,
+                'message': message
+            });
+        }
+        callback(null);
+        return null;
+    };
+    self.subscribe = function subscribe(channel, callback) {
+        if (!_.contains(subscriptions, channel)) {
+            subscriptions.push(channel);
+        }
+        callback(null);
+        return null;
+    };
+}
+// complete the inheritance
+util.inherits(Service, events.EventEmitter);
+// define an explicit creator function
+function create_service() {
+    return new Service();
+}
+
+// [ Exports ]
+exports.create_service = create_service;

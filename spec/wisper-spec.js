a28e6eee5e7054ffd64023770be6e28202b793f7
toejough <toejough@gmail.com>
2014-07-31 23:21:49 -0600

    0.3.0 - something's wrong with the teardown, and there are 'null' messages being printed which should not be, and there are no negative tests, but client/server pub/sub with regex is working

--- spec/wisper-spec.js
+++ spec/wisper-spec.js
@@ -7,10 +7,14 @@
 /* global describe */
 /* global it */
 /* global beforeEach */
+/* global afterEach */
 /* global expect */
 
 
 // [ Requires ]
+// [ -Third Party- ]
+var _ = require('underscore');
+// [ -Project- ]
 var wisper = require('../wisper.js');
 
 
@@ -106,6 +110,211 @@ describe('local pub/sub', function() {
         service.publish({
             'channel': 'foo',
             'message': 'bar'
+        }, function(error) {
+            expect(error).toBeFalsy();
+            return null;
+        });
+        return null;
+    });
+});
+describe('remote pub/sub', function() {
+    // vars
+    var server;
+    var client_1;
+    var client_2;
+    // setup/teardown
+    beforeEach(function(done) {
+        server = wisper.create_server(8889, function(error) {
+            expect(error).toBeFalsy();
+            client_1 = wisper.create_client(8889, '127.0.0.1');
+            client_1.on('open', function() {
+                client_2 = wisper.create_client(8889, '127.0.0.1');
+                client_2.on('open', done);
+            });
+        });
+        return null;
+    });
+    afterEach(function(done) {
+        client_1.close();
+        client_2.close();
+        server.close(done);
+    });
+    // tests
+    it('allows remote publish', function(done) {
+        expect(client_1.publish).toBeDefined();
+        client_1.publish({
+            'channel': 'foo',
+            'message': 'bar'
+        }, function(error) {
+            expect(error).toBeFalsy();
+            done();
+            return null;
+        });
+        return null;
+    });
+    it('allows remote subscribe', function(done) {
+        expect(client_2.subscribe).toBeDefined();
+        client_2.subscribe('foo', function(error) {
+            expect(error).toBeFalsy();
+            done();
+            return null;
+        });
+        return null;
+    });
+    it('delivers publications to remote subscribers', function(done) {
+        client_2.subscribe('foo', function(error) {
+            expect(error).toBeFalsy();
+            return null;
+        });
+        client_2.on('publication', function(publication) {
+            expect(publication).toBeTruthy();
+            expect(publication.channel).toEqual('foo');
+            expect(publication.message).toEqual('bar');
+            done();
+            return null;
+        });
+        client_1.publish({
+            'channel': 'foo',
+            'message': 'bar'
+        }, function(error) {
+            expect(error).toBeFalsy();
+            return null;
+        });
+        return null;
+    });
+    it('honors regex .* subscriptions for remote subscribers', function(done) {
+        var regex = '.*';
+        client_2.subscribe(regex, function(error) {
+            expect(error).toBeFalsy();
+            return null;
+        });
+        client_2.on('publication', function(publication) {
+            expect(publication).toBeTruthy();
+            expect(publication.channel).toEqual('foo');
+            expect(publication.message).toEqual('bar');
+            expect(publication.subscriptions).toEqual([regex]);
+            done();
+            return null;
+        });
+        client_1.publish({
+            'channel': 'foo',
+            'message': 'bar'
+        }, function(error) {
+            expect(error).toBeFalsy();
+            return null;
+        });
+        return null;
+    });
+    it('honors regex ? subscriptions for remote subscribers', function(done) {
+        var regex = 'f(?:oo)?';
+        client_2.subscribe(regex, function(error) {
+            expect(error).toBeFalsy();
+            return null;
+        });
+        client_2.on('publication', function(publication) {
+            expect(publication).toBeTruthy();
+            expect(publication.channel).toEqual('foo');
+            expect(publication.message).toEqual('bar');
+            expect(publication.subscriptions).toEqual([regex]);
+            done();
+            return null;
+        });
+        client_1.publish({
+            'channel': 'foo',
+            'message': 'bar'
+        }, function(error) {
+            expect(error).toBeFalsy();
+            return null;
+        });
+        return null;
+    });
+    it('delivers publications to all subscribers', function(done) {
+        var sync_sources = {1: false, 2: false};
+        function sync(x) {
+            sync_sources[x] = true;
+            if (_.every(sync_sources)) {
+                done();
+            }
+            return null;
+        }
+        client_2.subscribe('foo', function(error) {
+            expect(error).toBeFalsy();
+            return null;
+        });
+        client_2.on('publication', function(publication) {
+            expect(publication).toBeTruthy();
+            expect(publication.channel).toEqual('foo');
+            expect(publication.message).toEqual('bar');
+            sync(2);
+            return null;
+        });
+        client_1.subscribe('foo', function(error) {
+            expect(error).toBeFalsy();
+            return null;
+        });
+        client_1.on('publication', function(publication) {
+            expect(publication).toBeTruthy();
+            expect(publication.channel).toEqual('foo');
+            expect(publication.message).toEqual('bar');
+            sync(1);
+            return null;
+        });
+        client_1.publish({
+            'channel': 'foo',
+            'message': 'bar'
+        }, function(error) {
+            expect(error).toBeFalsy();
+            return null;
+        });
+        return null;
+    });
+    it('delivers publications to only subscribers', function(done) {
+        var sync_sources = {1: false, 2: false};
+        function sync(x) {
+            sync_sources[x] = true;
+            if (_.every(sync_sources)) {
+                done();
+            }
+            return null;
+        }
+        client_2.subscribe('foo', function(error) {
+            expect(error).toBeFalsy();
+            return null;
+        });
+        client_2.on('publication', function(publication) {
+            expect(publication).toBeTruthy();
+            expect(publication.channel).toEqual('foo');
+            expect(publication.message).toEqual('bar');
+            if (publication.channel == 'baz') {
+                throw new Error('got message for channel not subscribed to');
+            }
+            sync(2);
+            return null;
+        });
+        client_1.subscribe('baz', function(error) {
+            expect(error).toBeFalsy();
+            return null;
+        });
+        client_1.on('publication', function(publication) {
+            expect(publication).toBeTruthy();
+            expect(publication.channel).toEqual('baz');
+            expect(publication.message).toEqual('quux');
+            if (publication.channel == 'foo') {
+                throw new Error('got message for channel not subscribed to');
+            }
+            sync(1);
+            return null;
+        });
+        client_1.publish({
+            'channel': 'foo',
+            'message': 'bar'
+        }, function(error) {
+            expect(error).toBeFalsy();
+            return null;
+        });
+        client_1.publish({
+            'channel': 'baz',
+            'message': 'quux'
         }, function(error) {
             expect(error).toBeFalsy();
             return null;
--- wisper.js
+++ wisper.js
@@ -5,6 +5,7 @@
 'use strict';
 /* global require */
 /* global exports */
+/* global console */
 
 
 // [ Requires ]
@@ -13,6 +14,7 @@ var events = require('events');
 var util = require('util');
 // [ -Third Party- ]
 var _ = require('underscore');
+var ws = require('ws');
 
 
 // [ Service ]
@@ -58,5 +60,117 @@ function create_service() {
     return new Service();
 }
 
+
+// [ Server ]
+function Server(port, callback) {
+    // [ -Private Vars- ]
+    // self is always the service instance
+    var self = this;
+    var WSServer = ws.Server;
+    var ws_server = new WSServer({'port': port}, callback);
+    var services = [];
+    var conns = [];
+
+    ws_server.on('connection', function(conn) {
+        // record the conn
+        conns.push(conn);
+        // get a service for this client
+        conn.service = create_service();
+        // add it to the overall list for the server
+        services.push(conn.service);
+        // send when something is published
+        conn.service.on('publication', function(publication) {
+            conn.send(JSON.stringify(publication));
+        });
+        // listen to messages
+        conn.on('message', function(message) {
+            var json_message = JSON.parse(message);
+            // subscribe to this service only.
+            if (json_message.type == 'subscribe') {
+                conn.service.subscribe(json_message.body.channel, function(error) {
+                    console.error(error);
+                });
+            // publish to all services
+            } else if (json_message.type == 'publish') {
+                _.each(services, function(service) {
+                    service.publish(json_message.body, function(error) {
+                        console.error(error);
+                    });
+                });
+            }
+        });
+        // remove service when this conn closes
+        conn.on('close', function() {
+            var index = services.indexOf(conn.service);
+            services.splice(index, 1);
+            index = conns.indexOf(conn);
+            conns.splice(index, 1);
+        });
+    });
+
+    // [ -Public- ]
+    self.close = function close(callback) {
+        _.each(conns, function(conn) {
+            conn.close();
+            conn.terminate();
+        });
+        ws_server.close();
+        callback(null);
+    };
+}
+// define an explicit creator function
+function create_server(port, callback) {
+    return new Server(port, callback);
+}
+
+
+// [ Client ]
+function Client(port, host) {
+    // [ -Private Vars- ]
+    // self is always the service instance
+    var self = this;
+    var services = [];
+    var ws_client = new ws('ws://' + host + ':' + port);
+
+    // [ -Inheritance- ]
+    events.EventEmitter.call(self);
+
+    ws_client.on('message', function(message) {
+        self.emit('publication', JSON.parse(message));
+    });
+    ws_client.on('open', function() {
+        self.emit('open');
+    });
+
+    // [ -Public- ]
+    self.publish = function publish(kwargs, callback) {
+        var message = {
+            'type': 'publish',
+            'body': kwargs
+        };
+        ws_client.send(JSON.stringify(message), callback);
+    };
+    self.subscribe = function subscribe(channel, callback) {
+        var message = {
+            'type': 'subscribe',
+            'body': { 'channel': channel }
+        };
+        ws_client.send(JSON.stringify(message), callback);
+    };
+    self.close = function close() {
+        ws_client.close();
+        ws_client.terminate();
+    };
+}
+// complete the inheritance
+util.inherits(Client, events.EventEmitter);
+// define an explicit creator function
+function create_client(port, host) {
+    return new Client(port, host);
+}
+
+
 // [ Exports ]
 exports.create_service = create_service;
+exports.create_server = create_server;
+exports.create_client = create_client;
